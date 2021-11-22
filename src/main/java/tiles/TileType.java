8fcff78c142104bed41dab0c463467d656d13e54
dandav8 <dandav8@gmail.com>
2021-11-22 11:39:01 -0600

    COLOR TESTS I spiked the code in TileType.java and levels/1.txt to see different tiles added.  Then I proceded to add a test in TileColorMapTest.java for the colors of the .COIN, .NEXT_LEVEL, .PREVIOUS_LEVEL tiles.  Then I added the code to pass these tests.

--- src/main/java/tiles/TileType.java
+++ src/main/java/tiles/TileType.java
@@ -2,7 +2,7 @@ package tiles;
 
 public enum TileType {
 
-	PASSABLE(' '), NOT_PASSABLE('X'), PLAYER('P');
+	PASSABLE(' '), NOT_PASSABLE('X'), PLAYER('P'), NEXT_LEVEL('N'), PREVIOUS_LEVEL('B'), COIN('C');
 
 	static final String INVALID_CHARACTER_PROVIDED_MESSAGE = "Invalid character provided: ";
 
--- src/main/java/values/TileColorMap.java
+++ src/main/java/values/TileColorMap.java
@@ -15,6 +15,9 @@ public final class TileColorMap {
 		tileColors.put(TileType.PASSABLE, Color.WHITE);
 		tileColors.put(TileType.NOT_PASSABLE, Color.BLACK);
 		tileColors.put(TileType.PLAYER, Color.GREEN);
+		tileColors.put(TileType.NEXT_LEVEL, Color.RED);
+		tileColors.put(TileType.PREVIOUS_LEVEL, Color.BLUE);
+		tileColors.put(TileType.COIN, Color.YELLOW);
 	}
 
 	public static Color get(TileType key) {
