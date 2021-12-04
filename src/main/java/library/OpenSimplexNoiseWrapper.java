51fbd38ed0f7590ac0442dd43cdd36d4a09d0a29
Timothy Ryer <tim@ryer.dev>
2021-12-04 14:30:32 -0500

    Added Library OpenSimplexNoise I added the library as a file since I couldn't find any maven repo that worked like this. There is a tested wrapper around the eval function that wraps around this library for producing good random level noise.

---
+++ src/main/java/library/OpenSimplexNoiseWrapper.java
@@ -0,0 +1,18 @@
+package library;
+
+public class OpenSimplexNoiseWrapper {
+
+	private OpenSimplexNoise noiseLibrary;
+
+	public OpenSimplexNoiseWrapper() {
+		noiseLibrary = new OpenSimplexNoise();
+	}
+
+	public OpenSimplexNoiseWrapper(long seed) {
+		noiseLibrary = new OpenSimplexNoise(seed);
+	}
+
+	public double eval(double x, double y) {
+		return noiseLibrary.eval(x, y);
+	}
+}
